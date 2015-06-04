158870798ccf5877ec997e6f79d67df6787de957
astraltear <astraltear@gmail.com>
2015-06-04 14:41:27 +0900

    by office

--- app/src/main/java/jee/young/androidtrainingdemo/MainActivity.java
+++ app/src/main/java/jee/young/androidtrainingdemo/MainActivity.java
@@ -17,6 +17,10 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        // if your minSdkVersion is 11 or higher,instead use:
+        // getActionBar().setDisplayShowHomeEnabled(true);
     }
 
     public void sendMessage(View view) {
@@ -39,10 +43,13 @@ public class MainActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
+
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            return true;
+        } else if(id==R.id.action_search){
             return true;
         }
 
