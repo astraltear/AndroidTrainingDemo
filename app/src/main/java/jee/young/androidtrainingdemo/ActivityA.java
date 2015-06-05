9b06624de70e4d673f969ac1fba89c76eea577af
astraltear <astraltear@gmail.com>
2015-06-05 16:25:47 +0900

    by office

--- app/src/main/java/jee/young/androidtrainingdemo/ActivityA.java
+++ app/src/main/java/jee/young/androidtrainingdemo/ActivityA.java
@@ -6,17 +6,29 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import jee.young.androidtrainingdemo.util.StatusTracker;
+import jee.young.androidtrainingdemo.util.Utils;
+
 
 public class ActivityA extends ActionBarActivity {
 
     String mActivityName;
     private TextView mStatusView;
     private TextView mStatusAllView;
-    private StatusTracker
+    private StatusTracker mStatusTracker = StatusTracker.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_a);
+
+        mActivityName = getString(R.string.activity_a);
+        mStatusView = (TextView) findViewById(R.id.status_view_a);
+        mStatusAllView = (TextView) findViewById(R.id.status_view_all_a);
+
+
+        mStatusTracker.setStatus(mActivityName,getString(R.string.on_create));
+
+        Utils.printStatus(mStatusView,mStatusAllView);
     }
 }
--- app/src/main/java/jee/young/androidtrainingdemo/util/StatusTracker.java
+++ app/src/main/java/jee/young/androidtrainingdemo/util/StatusTracker.java
@@ -1,7 +1,10 @@
 package jee.young.androidtrainingdemo.util;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class StatusTracker {
     private Map<String, String> mStatusMap;
@@ -11,5 +14,44 @@ public class StatusTracker {
 
     public static StatusTracker getInstance() {
         return ourInstance;
+    }
+
+    private StatusTracker() {
+        mStatusMap = new LinkedHashMap<String, String>();
+        mMethodList = new ArrayList<String>();
+    }
+
+    public List<String> getMethodList() {
+        return mMethodList;
+    }
+
+    public void clear(){
+        mMethodList.clear();
+        mStatusMap.clear();
+    }
+
+    public void setStatus(String activityName,String status){
+        mMethodList.add(activityName + "." + status + "()");
+        if(mStatusMap.containsKey(activityName)) {
+            mStatusMap.remove(activityName);
+        }
+
+        mStatusMap.put(activityName, status);
+    }
+
+    public String getStatus(String activityName) {
+        String status = mStatusMap.get(activityName);
+        status = status.substring(2, status.length());
+
+        if (status.endsWith("e")) {
+            status = status.substring(0, status.length() - 1);
+        }
+
+        status = status + STATUS_SUFFIX;
+        return status;
+    }
+
+    public Set<String> keySet() {
+        return mStatusMap.keySet();
     }
 }
